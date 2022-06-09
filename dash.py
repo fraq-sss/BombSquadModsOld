18fb942bf93a6488f1f208391bf65cc1293add57
exsinus <98089449+exsinus@users.noreply.github.com>
2022-06-09 22:15:39 +0600

    Update dash.py

--- dash.py
+++ dash.py
@@ -1,32 +1,33 @@
 import ba
 from bastd.actor.spaz import Spaz
 
-dash = True
+Spaz.dash = True
 
-def new_pickup_release(self) -> None:
-    old_pickup_release(self)
-    time_ms = ba.time(timeformat=ba.TimeFormat.MILLISECONDS)
+def new_punch_release(self) -> None:
+    old_punch_release(self)
+    time = ba.time(timeformat=ba.TimeFormat.MILLISECONDS)
+    spaz = self.node
     def impulse() -> None:
-        self.node.handlemessage("impulse",
-        self.node.position[0],
-        self.node.position[1],
-        self.node.position[2],
-        self.node.move_left_right * 30,
-        self.node.position[1] + 5,
-        self.node.move_up_down * -30, 5, 5, 0, 0,
-        self.node.move_left_right * 30,
-        self.node.position[1] + 5,
-        self.node.move_up_down * -30)
+        spaz.handlemessage("impulse",
+        spaz.position[0],
+        spaz.position[1],
+        spaz.position[2],
+        spaz.move_left_right * 30,
+        spaz.position[1] + 5,
+        spaz.move_up_down * -30, 5, 5, 0, 0,
+        spaz.move_left_right * 30,
+        spaz.position[1] + 5,
+        spaz.move_up_down * -30)
 
-    if not dash \
-    or not self.node.exists() \
-    or self.node.knockout > 0.0 \
-    or self.node.frozen > 0.0 \
-    or time_ms - self.last_pickup_time_ms < 800:
+    if not self.dash \
+    or not spaz.exists() \
+    or spaz.knockout > 0.0 \
+    or spaz.frozen > 0.0 \
+    or time - self.last_punch_time_ms < 800:
         return
     ba.emitfx(
-        position=self.node.position,
-        velocity=self.node.velocity,
+        position=spaz.position,
+        velocity=spaz.velocity,
         count=50,
         scale=0.5,
         spread=1.0,
@@ -36,8 +37,8 @@ def new_pickup_release(self) -> None:
         ba.timer(i * 0.01, impulse)
         ba.playsound(ba.getsound("shieldHit"))
 
-old_pickup_release = Spaz.on_pickup_release
-Spaz.on_pickup_release = new_pickup_release
+old_punch_release = Spaz.on_punch_release
+Spaz.on_punch_release = new_punch_release
 
 # ba_meta require api 6
 # ba_meta export plugin
