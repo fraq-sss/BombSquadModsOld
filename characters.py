273da39dc4f4f317faa4fe01229105496fc8bbe0
exsinus <98089449+exsinus@users.noreply.github.com>
2022-07-23 08:37:57 +0600

    Create characters.py
    Новые персонажи.
    - Tолько на серверах со сборкой Mirage!

---
+++ characters.py
@@ -0,0 +1,12 @@
+from ba import Plugin
+from bastd.actor.spazappearance import Appearance
+
+
+names = (
+    "Twins", "Mosquito", "Pyramid Head", "Turtle", "Dino", "Lollipop",
+    "Hunter", "Impact", "Elemental", "Frostyk", "Frost"
+)
+
+
+for name in names:
+	Appearance(name)
