f383e48fc952613e551b24a4b6efa276fa86d3cd
fraq-sss <98089449+fraq-sss@users.noreply.github.com>
2023-04-15 15:55:35 +0600

    Update characters.py

--- characters.py
+++ characters.py
@@ -1,11 +1,9 @@
 from bastd.actor.spazappearance import Appearance
 
-
 names = (
     "Twins", "Mosquito", "Pyramid Head", "Turtle", "Dino", "Lollipop",
     "Hunter", "Impact", "Elemental", "Frostyk", "Frost"
 )
-
 
 for name in names:
     Appearance(name)
