041cfa588c93f42392920f16a5234746c68e5de3
fraq-sss <98089449+fraq-sss@users.noreply.github.com>
2023-06-08 07:28:54 +0600

    Update characters.py

--- characters.py
+++ characters.py
@@ -1,14 +1,21 @@
+# ba_meta require api 7
+
 """Characters/Skins
 
 Working only in servers with the Abyss server script.
 """
 
+from ba import Plugin
 from bastd.actor.spazappearance import Appearance
 
-names = (
+NAMES = (
     "Twins", "Mosquito", "Pyramid Head", "Turtle", "Dino", "Lollipop",
     "Hunter", "Impact", "Elemental", "Frostyk", "Frost"
 )
 
-for name in names:
-    Appearance(name)
+
+# ba_meta export plugin
+class ServerCharacters(Plugin):
+    def on_app_running(self) -> None:
+        for name in NAMES:
+            Appearance(name)
